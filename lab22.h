563808a287b8aabaabd83b1fd001783ed36631ef
Pubet Choompusueb <pubet50@gmail.com>
2022-02-04 16:48:19 +0700

    completed

--- lab22.h
+++ lab22.h
@@ -63,13 +63,41 @@ void Unit::newTurn(){
 	guard_on = false;
 }
 
+bool Unit::isDead(){
+	if(hp<=0) return true;
+	else return false;
+}
 
+void Unit::guard(){
+	guard_on=true;
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-//Write function members isDead(), guard(), heal(), beAttacked(), and attack() here//
-/////////////////////////////////////////////////////////////////////////////////////
+int Unit::beAttacked(int oppatk){
+	int dmg=oppatk-def;
+	if(guard_on==false){
+		hp=hp-dmg;
+		return dmg;
+	}else{
+		hp=hp-(dmg/3);
+		return dmg/3;
+	}
+}
 
+int Unit::attack(Unit &target){
+	return target.beAttacked(atk);
+}
 
+int Unit::heal(){
+	int heal = rand()%21+10;
+	if(hp+heal>=hpmax){
+		int hpnow=hp;
+		hp=hpmax;
+		return hpmax-hpnow;
+	}else{
+		hp=hp+heal;
+		return heal;
+	}
+}
 
 void drawScene(char p_action,int p,char m_action,int m){
 	cout << "                                                       \n";
